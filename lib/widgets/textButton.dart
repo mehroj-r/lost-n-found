534917803937950d07b4e6e6a9f26d3014df96a7
Ollayorbek <ollayorbek2005@gmail.com>
2025-11-01 16:26:03 +0500

    set up

---
+++ lib/widgets/textButton.dart
@@ -0,0 +1,13 @@
+import 'package:flutter/material.dart';
+
+class OurButton extends StatelessWidget {
+  const OurButton({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return InkWell(
+      onTap: (){},
+      child: Text("OurButton"),
+    );
+  }
+}
