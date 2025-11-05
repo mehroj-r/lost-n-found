8385e1c22ffbc988815daaf3e4f4f3ee394b0e38
MEHROJ <mekhrojmajidov@gmail.com>
2025-11-05 17:01:54 +0500

    feat(post): add like functionality to posts
    - enable users to like and unlike posts
    - add like_count field to track the number of likes
    - implement like_post action to handle liking and unliking

--- src/apps/post/api/views.py
+++ src/apps/post/api/views.py
@@ -1,5 +1,8 @@
+from django.db import transaction
 from drf_spectacular.utils import extend_schema
-from rest_framework import viewsets
+from rest_framework import viewsets, status
+from rest_framework.decorators import action
+from rest_framework.response import Response
 
 from apps.core.api.views.base import BaseAPIView
 from apps.post.api.serializers import PostSerializer
@@ -14,3 +17,31 @@ class PostAPIViewSet(BaseAPIView, viewsets.ModelViewSet):
     def list(self, request, *args, **kwargs):
         self.queryset = self.queryset.select_related('author')
         return super().list(request, *args, **kwargs)
+
+    @transaction.atomic
+    @action(methods=['POST', 'DELETE'], detail=True, url_path='likes', url_name='like_post')
+    def like_post(self, request, pk=None):
+
+        if request.method == 'DELETE':
+            return self._unlike_post(request, pk)
+        elif request.method == 'POST':
+            return self._like_post(request, pk)
+        else:
+            return Response({'detail': 'Method not allowed.'}, status=status.HTTP_405_METHOD_NOT_ALLOWED)
+
+
+    def _like_post(self, request, pk=None):
+        post = self.get_object()
+        post.likes.add(request.user)
+        post.like_count += post.likes.count()
+        post.save()
+        serializer = self.get_serializer(post)
+        return Response(serializer.data, status=status.HTTP_201_CREATED)
+
+    def _unlike_post(self, request, pk=None):
+        post = self.get_object()
+        post.likes.remove(request.user)
+        post.like_count = post.likes.count()
+        post.save()
+        serializer = self.get_serializer(post)
+        return Response(serializer.data, status=status.HTTP_204_NO_CONTENT)
--- src/apps/post/models.py
+++ src/apps/post/models.py
@@ -12,7 +12,15 @@ class Post(TimestampedModel):
     photo = models.ForeignKey(to="file.File", on_delete=models.SET_NULL, null=True, blank=True, related_name="posts")
     is_completed = models.BooleanField(default=False)
     like_count = models.PositiveIntegerField(default=0)
-    likes = models.ManyToManyField(to="account.User", related_name="liked_posts", blank=True)
+    likes = models.ManyToManyField(to="account.User", related_name="liked_posts", blank=True, through="PostLikes")
 
     def __str__(self):
         return self.title
+
+
+class PostLikes(TimestampedModel):
+    user = models.ForeignKey(to="account.User", on_delete=models.CASCADE)
+    post = models.ForeignKey(to="post.Post", on_delete=models.CASCADE)
+
+    class Meta:
+        unique_together = ('user', 'post')
