c81c8aab9a8088af195a79acb79ec94af1d3017d
MEHROJ <mekhrojmajidov@gmail.com>
2025-11-21 17:22:19 +0500

    feat(notification): add read notification functionality
    - add api endpoint to mark a notification as read
    - add api endpoint to mark all notifications as read
    - add is_read field in notification serializer

--- src/apps/notification/api/serializers.py
+++ src/apps/notification/api/serializers.py
@@ -4,12 +4,14 @@ from apps.notification.models import Notification
 
 
 class NotificationSerializer(serializers.ModelSerializer):
+    is_read = serializers.BooleanField(read_only=True)
 
     class Meta:
         model = Notification
         fields = [
             'id',
             'title',
+            'is_read',
             'message',
             'created_at'
         ]
--- src/apps/notification/api/views.py
+++ src/apps/notification/api/views.py
@@ -1,5 +1,7 @@
 from django.db.models import Q
-from rest_framework import viewsets, permissions
+from rest_framework import viewsets, permissions, status
+from rest_framework.decorators import action
+from rest_framework.response import Response
 
 from apps.core.api.views.base import BaseAPIView
 from apps.core.utils.constants import NotificationBroadcast
@@ -15,5 +17,28 @@ class NotificationViewSet(BaseAPIView, viewsets.ModelViewSet):
     def list(self, request, *args, **kwargs):
         self.queryset = self.get_queryset().filter(
             Q(users=request.user) | Q(broadcast_type=NotificationBroadcast.ALL)
+        ).annotate(
+            is_read=~Q(notificationuser__user=request.user, notificationuser__is_read=False)
         ).order_by('-created_at')
         return super().list(request, *args, **kwargs)
+
+
+    @action(methods=['POST'], detail=True, url_path='read', url_name='read_notification')
+    def read(self, request, *args, **kwargs):
+        notification = self.get_object()
+        notification_user, created = notification.notificationuser_set.get_or_create(user=request.user)
+        notification_user.is_read = True
+        notification_user.save()
+        return Response({'detail': 'Notification marked as read.'}, status=status.HTTP_200_OK)
+
+
+    @action(methods=['POST'], detail=False, url_path='read-all', url_name='readl_all_notifications')
+    def read_all(self, request, *args, **kwargs):
+        notifications = self.get_queryset().filter(
+            Q(users=request.user) | Q(broadcast_type=NotificationBroadcast.ALL)
+        )
+        for notification in notifications:
+            notification_user, created = notification.notificationuser_set.get_or_create(user=request.user)
+            notification_user.is_read = True
+            notification_user.save()
+        return Response({'detail': 'All notifications marked as read.'}, status=status.HTTP_200_OK)
